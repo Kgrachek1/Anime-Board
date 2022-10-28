0565d56ce238854352505633ea36e7bf167fcd1f
Kgchek <rptr22f@msn.com>
2022-10-28 11:34:46 -0700

    Passport and Oauth add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
-/* GET home page. */
+// This app has no "home" page, 
+// but your projects should 😀
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/users');
+});
+
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    scope: ['profile', 'email'],
+    // Optional
+    prompt: 'select_account'
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/home',
+    // Change to what's best for YOUR app
+    failureRedirect: '/users'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout(function() {
+    // Change path for your "landing" page
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
