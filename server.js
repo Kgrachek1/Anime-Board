4e6bbf88de017668621498353768efe8e92cafe6
Kgchek <rptr22f@msn.com>
2022-10-28 10:49:52 -0700

    Connect to database

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var methodOverride = require('method-override');
+
+require('dotenv').config();
+require('./config/database');
 
 var app = express();
 
