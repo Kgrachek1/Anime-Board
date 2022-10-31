204410cc070897e9843840a88656fb9921ef4efe
Kgchek <rptr22f@msn.com>
2022-10-31 16:32:22 -0700

    Changes

--- routes/creators.js
+++ routes/creators.js
@@ -1,15 +1,19 @@
+
 const express = require('express');
 const router = express.Router();
 const creatorsCtrl = require('../controllers/creators');
 const ensureLoggedIn = require('../config/auth');
 
-// This router is mounted to a "starts with" path of '/'
 
-// GET /performers/new
+
 router.get('/creators/new', ensureLoggedIn, creatorsCtrl.new);
-// POST /performers
+
 router.post('/creators/new', ensureLoggedIn, creatorsCtrl.create);
-// POST /movies/:id/performers (assoc movie & performer)
+
 router.post('/animes/:id/creators', ensureLoggedIn, creatorsCtrl.addToCast);
-router.get('/creators/api', ensureLoggedIn)
+
+
+
+
+
 module.exports = router;
