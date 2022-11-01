5a0eba7c097cc935243c19a5ab78ce8a975d9a3d
Kgchek <rptr22f@msn.com>
2022-11-01 15:23:48 -0700

    second style sheet

--- api.js
+++ api.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var router = express.Router();
-const ROOT_URL = 'https://api.jikan.moe/v4';
+var api = require('./api.js')
 
+const ROOT_URL = 'https://api.jikan.moe/v4/anime';
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   const anime = req.query.anime;
-  if (!anime) return res.render('index', { animeData: null });
-  const animeData = await fetch(`${ROOT_URL}/users/${anime}`)
+  if (!anime) return res.render('new', { animeData: null });
+  const animeData = await fetch(`${ROOT_URL}/anime/${anime}`)
     .then(res => res.json());
-  animeData.search = await fetch(animeData.search_url)
+  animeData.anime = await fetch(animeData.ROOT_URL)
     .then(res => res.json());
-  res.render('index', { animeData });
+  res.render('new', { animeData });
+  // let animeData;
+  // fetch(`${ROOT_URL}/users/${username}`, options)
+  //   .then(res => res.json())
+  //   .then(userInfo => {
+  //     userData = userInfo;
+  //     return fetch(userData.repos_url, options);
+  //   })
+  //   .then(res => res.json())
+  //   .then(repos => {
+  //     console.log(repos[0]);
+  //     res.render('index', { userData });
+  //   })
 });
-
-
 
 module.exports = router;
--- routes/animes.js
+++ routes/animes.js
@@ -3,15 +3,13 @@ const router = express.Router();
 const animeCtrl = require('../controllers/animes');
 const ensureLoggedIn = require('../config/auth');
 
-// All routes start with '/movies'
 
-// GET /movies (display all movies)
 router.get('/', animeCtrl.index);
-// GET /movies/new (display a form for entering a new movie)
+
 router.get('/new', ensureLoggedIn, animeCtrl.new);
-// GET /movies/:id (display a "detail/show" page for a single movie)
+
 router.get('/:id', animeCtrl.show);
-// POST /movies (handle the new form being submitted)
+
 router.post('/', ensureLoggedIn, animeCtrl.create);
 
 module.exports = router;
